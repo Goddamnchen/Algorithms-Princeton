acca930dbf7f3fd4679a365c2b8ce1c60183e5f5
belkacem_tpc <belkacem.lahouel@gmail.com>
2016-09-06 17:15:10 +0200

    Adding Percolation class. PercolationStats to do.

---
+++ 11/src/PercolationStats.java
@@ -0,0 +1,52 @@
+
+import edu.princeton.cs.algs4.*;
+
+public class PercolationStats {
+
+	/**
+	 * Performs trials (independent experiments) on an n-by-n grid
+	 * @param n size of the grid
+	 * @param trials number of experiments
+	 */
+	public PercolationStats(int n, int trials) {
+		if (n < 1 || trials < 1)
+			throw new java.lang.IllegalArgumentException();
+		
+		//TODO
+	}
+	
+	/**
+	 * sample mean of percolation threshold
+	 * @return mean of percolation threshold
+	 */
+	public double mean() {
+		//TODO
+		return 0;
+	}
+	
+	/**
+	 * low endpoint of 95% confidence interval
+	 * @return low endpoint of 95% confidence interval
+	 */
+	public double confidenceLo() {
+		//TODO
+		return 0;
+	}
+	
+	/**
+	 * high endpoint of 95% confidence interval
+	 * @return high endpoint of 95% confidence interval
+	 */
+	public double confidenceHi() {
+		//TODO
+		return 0;
+	}
+	
+	/**
+	 * test client
+	 * @param args [n size of the grid, trials number of experiments]
+	 */
+	public static void main(String[] args) {
+		//TODO
+	}
+}

558e043ebedd22d775d02c0493d997c9c1a83b11
belkacem_tpc <belkacem.lahouel@gmail.com>
2016-09-14 14:01:45 +0200

    Test

--- 13/src/PointsWindow.java
+++ 13/src/PointsWindow.java
@@ -3,7 +3,8 @@ import edu.princeton.cs.algs4.StdDraw;
 public class PointsWindow {
 	
 	public static void main(String[] args) {
-		Point[] points = {new Point(10000, 0), new Point(0, 10000), new Point(3000, 7000), new Point(7000, 3000), new Point(20000, 21000), new Point(3000, 4000), new Point(14000, 15000), new Point(6000, 7000)};
+		//Point[] points = {new Point(10000, 0), new Point(0, 10000), new Point(3000, 7000), new Point(7000, 3000), new Point(20000, 21000), new Point(3000, 4000), new Point(14000, 15000), new Point(6000, 7000)};
+		Point[] points = {new Point(10000,200), new Point(2000,10000), new Point(2000,20000), new Point(2000,5000), new Point(2000,8000)};
 		
 		StdDraw.enableDoubleBuffering();
 		StdDraw.setXscale(0, 32768);

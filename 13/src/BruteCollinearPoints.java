f56cf3ee4596a344c8d3da472c92b2ce6ff0cf34
belkacem_tpc <belkacem.lahouel@gmail.com>
2016-09-14 12:03:41 +0200

    Improvements in for loops + exceptions

--- 13/src/BruteCollinearPoints.java
+++ 13/src/BruteCollinearPoints.java
@@ -3,26 +3,55 @@ import java.util.Arrays;
 
 public class BruteCollinearPoints {
 	
-	private static final double epsilon = 0.000001;
+	private static final double epsi = 0.001;
 	
+	private class MyLineSegment {
+				
+		public Point p1, p2;
+		
+		public MyLineSegment(Point p1, Point p2) {
+			this.p1 = p1;
+			this.p2 = p2;
+		}
+	}
+		
 	private LineSegment[] segments;
 	
 	public BruteCollinearPoints(Point[] points) {
 		if (points == null)
 			throw new java.lang.NullPointerException();
 		
-		ArrayList<LineSegment> tmp = new ArrayList<LineSegment>();
+		for (Point p : points)
+			if (p == null)
+				throw new java.lang.NullPointerException();
 		
-		for (Point p : points) {
-			for (Point q : points) {
-				for (Point r : points) {
-					for (Point s : points) {
+		for (int i = 0; i < points.length; ++i)
+			for (int j = i+1; j < points.length; ++j)
+				if (points[i].compareTo(points[j]) == 0)
+					throw new java.lang.IllegalArgumentException();
+		
+		//--- checks done
+		
+		ArrayList<MyLineSegment> tmp = new ArrayList<MyLineSegment>();
+		
+		for (int a = 0; a < points.length; ++a) {
+			Point p = points[a];
+			
+			for (int b = a+1; b < points.length; ++b) {
+				Point q = points[b];
+				
+				for (int c = b+1; c < points.length; ++c) {
+					Point r = points[c];
+					
+					for (int d = c+1; d < points.length; ++d) {
+						Point s = points[d];
+						
 						if ((p != q && p != r && p != s)) {
 							double slopePQ = p.slopeTo(q);
 							double slopePR = p.slopeTo(r);
 							double slopePS = p.slopeTo(s);
 							
-							if (Math.abs(slopePQ - slopePR) < epsilon && Math.abs(slopePQ - slopePS) < epsilon) {
+							if (slopePQ - slopePR < epsi && slopePQ - slopePS < epsi) {
 								Point[] four = new Point[4];
 								four[0] = p;
 								four[1] = q;
@@ -30,8 +59,9 @@ public class BruteCollinearPoints {
 								four[3] = s;
 								Arrays.sort(four);
 								
-								LineSegment seg = new LineSegment(four[0], four[3]);
-								tmp.add(seg);
+								MyLineSegment seg = new MyLineSegment(four[0], four[3]);
+								
+								addLS(tmp, seg);
 							}
 						}
 					}
@@ -39,7 +69,16 @@ public class BruteCollinearPoints {
 			}
 		}
 		
-		segments = tmp.toArray(new LineSegment[tmp.size()]);
+		//--- storage
+		
+		segments = new LineSegment[tmp.size()];
+		for (int i = 0; i < tmp.size(); ++i) {
+			segments[i] = new LineSegment(tmp.get(i).p1, tmp.get(i).p2);
+		}
+	}
+	
+	private void addLS(ArrayList<MyLineSegment> list, MyLineSegment seg) {
+		list.add(seg);
 	}
 	
 	public int numberOfSegments() {

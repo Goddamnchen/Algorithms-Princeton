15720f8e964da20edbaaa748973c4f398eeb26bd
belkacem_tpc <belkacem.lahouel@gmail.com>
2016-09-29 16:23:25 +0200

    Error with gitignore + Solver done, need bug cor (unsolvables don't work)

--- 14/src/Solver.java
+++ 14/src/Solver.java
@@ -1,35 +1,101 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Stack;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
 	
-	private final Board initial;
-	private boolean isSolvable = false;
-	private List<Board> solution = new LinkedList<>();
-	private int moves = 0;
+	private class Node implements Comparable<Node> {
+		private Board board;
+		private Node prev;
 
-	public Solver(Board _initial) {
-		initial = _initial;
+		private int moves;
+		private int manhattan;
+		private int priority;
 		
-		//Compute solution here
+		private Node(Board b, Node p) {
+			board = b;
+			prev = p;
+			
+			moves = (prev == null) ? 0 : prev.moves+1;
+			manhattan = board.manhattan();
+			priority = manhattan + moves;
+		}
+		
+		@Override
+		public int compareTo(Node that) {
+			if (priority > that.priority)
+				return 1;
+			else if (priority < that.priority)
+				return -1;
+			else
+				return 0;
+		}
+	}
+
+	private Node sol;
+	private Node twinsol;
+	
+	public Solver(Board initial) {
+		sol = new Node(initial, null);
+		MinPQ<Node> pq = new MinPQ<>();
+		pq.insert(sol);
+		
+		//---
+		
+		twinsol = new Node (initial.twin(), null);
+		MinPQ<Node> twinpq = new MinPQ<>();
+		twinpq.insert(twinsol);
+		
+		while (!sol.board.isGoal()) {
+			sol = pq.delMin();
+			Iterable<Board> neighbors = sol.board.neighbors();
+			
+			for (Board neigh : neighbors) {
+				if (!sol.board.equals(neigh))
+					pq.insert(new Node(neigh, sol));
+			}
+			
+			//---
+			
+			twinsol = twinpq.delMin();
+			Iterable<Board> twinneighbors = twinsol.board.neighbors();
+			
+			for (Board neigh : twinneighbors) {
+				if (!twinsol.board.equals(neigh))
+					twinpq.insert(new Node(neigh, twinsol));
+			}
+			
+			if (twinsol.board.isGoal()) {
+				sol = null;
+				break;
+			}
+		}
 	}
 	
 	public boolean isSolvable() {
-		return isSolvable;
+		return (sol != null);
 	}
 	
 	public int moves() {
 		if (!isSolvable())
 			return -1;
 		
-		return moves;
+		return sol.moves;
 	}
 	
 	public Iterable<Board> solution() {
-		return solution;
+		if (!isSolvable())
+			return null;
+		
+		Stack<Board> ans = new Stack<>();
+		Node tmp = sol;
+		while (tmp != null) {
+			ans.push(tmp.board);
+			tmp = tmp.prev;
+		}
+		return ans;
 	}
 	
 	public static void main(String[] args) {

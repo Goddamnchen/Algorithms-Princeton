7f86f234051184d8dc00c09fa7baa31e6225e2a6
belkacem_tpc <belkacem.lahouel@gmail.com>
2016-09-29 11:37:54 +0200

    Week4 8-Puzzle

---
+++ 14/src/Board.java
@@ -0,0 +1,66 @@
+
+public class Board {
+
+	private int[][] blocks;
+	private final int n;
+	
+	public Board(int[][] _blocks) {
+		n = _blocks.length;
+		
+		blocks = new int[n][n];
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				blocks[i][j] = _blocks[i][j];
+			}
+		}
+	}
+	
+	public int dimension() {
+		return n;
+	}
+	
+	public int hamming() {
+		int k = 1;
+		int dist = 0;
+		
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (blocks[i][j] != 0 && blocks[i][j] != k)
+					++dist;
+				++k;
+			}
+		}
+		
+		return dist;
+	}
+	
+	public int manhattan() {
+		int k = 1;
+		int dist = 0;
+		
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (blocks[i][j] != 0) {
+					int tmp = Math.abs(k - blocks[i][j]);
+					dist += tmp/3 + tmp%3;
+				}
+				++k;
+			}
+		}
+		
+		return dist;
+	}
+	
+	@Override
+	public String toString() {
+		String str = "";
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n-1; ++j) {
+				str += blocks[i][j] + " ";
+			}
+			str += blocks[i][n-1];
+			if (i < n-1) str += "\n";
+		}
+		return str;
+	}
+}
---
+++ 14/src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		Board board_solved = new Board(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}});
+		System.out.println(board_solved);
+		System.out.println("Man: " + board_solved.manhattan());
+		System.out.println("Ham: " + board_solved.hamming());
+		
+		System.out.println("\n---\n");
+		
+		Board board1 = new Board(new int[][] {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}});
+		System.out.println(board1);
+		System.out.println("Man: " + board1.manhattan());
+		System.out.println("Ham: " + board1.hamming());
+	}
+}

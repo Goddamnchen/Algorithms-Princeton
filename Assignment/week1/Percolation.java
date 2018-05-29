742e87852089de429d790cc7160aa50127b517c6
陈冠廷 <goddamnchen@chenguantingdeMacBook-Pro.local>
2018-05-30 00:25:43 +0800

    accomplished Percolation and fixed the bug of backwater by creating a new WQUUF

--- Assignment/week1/Percolation.java
+++ Assignment/week1/Percolation.java
@@ -1,75 +1,144 @@
-import edu.princeton.cs.algs4.StdDraw;
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
+/***************************************************
+ *   Author: Guanting Chen
+ *   Date: 05/29/2018
+ *   Union-Find Assignment
+ *   Percolation API using by PercolationVisualizer
+ ***************************************************/
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.lang.Exception;
 
 public class Percolation {
     private WeightedQuickUnionUF site;
     private int size;
     private int nOS;        //number of Open Sites
-    private boolean[] siteStatus;
+    private boolean[] isOpenSite;
+    private WeightedQuickUnionUF connectTop;
 
     public Percolation(int n) {      // create n-by-n grid, with all sites blocked
         if (n <= 0) throw new IllegalArgumentException("n can not < 0");
-        site = new WeightedQuickUnionUF(n^2 + 2);       // implement index [0,n^2 +1]
+        site = new WeightedQuickUnionUF(n * n + 2);       // implement index [0,n^2 +1]
+        connectTop = new WeightedQuickUnionUF(n * n + 2);
         size = n;
-        siteStatus = initSiteStatus(n^2 + 2);
+        initSiteStatus(n * n + 2);
     }
     /**
-     * label a open site (row, col) if it is not open already
-     * union site?
+     * 1.check indices
+     * 2.label a open site (row, col) if it is not open already
+     * 3.union surround site
      */
     public void open(int row, int col){
         validate(row, col);
         if (!isOpen(row, col)){
             int index = xyTo1d(row, col);
-            siteStatus[index] = true;
+            isOpenSite[index] = true;
+            unionSelection(row, col, index);       //1<=index<=size*size 2;
             nOS ++;
         }
     }
-    public boolean isOpen(int row, int col){        // is site (row, col) open?
+
+    /**
+     * union site according to the site position
+     * dividing by Top or Bottom site conditional branch
+     */
+    private void unionSelection(int x, int y, int index) {
+        if (x == 1) {
+            if (this.isOpen(x + 1, y)) {          //下
+                site.union(xyTo1d(x + 1,y), index);
+                connectTop.union(xyTo1d(x + 1,y), index);
+            }
+            unionSelectionLR(x,y,index);
+        } else if (x == size) {
+            if (this.isOpen(x - 1, y)) {        //上
+                site.union(xyTo1d(x - 1,y), index);
+                connectTop.union(xyTo1d(x - 1,y), index);
+            }
+            unionSelectionLR(x,y,index);
+        } else {        //上下
+            if (this.isOpen(x - 1, y)) {
+                site.union(xyTo1d(x - 1,y), index);
+                connectTop.union(xyTo1d(x - 1,y), index);
+            }
+            if (this.isOpen(x + 1, y)) {
+                site.union(xyTo1d(x + 1,y), index);
+                connectTop.union(xyTo1d(x + 1,y), index);
+            }
+            unionSelectionLR(x,y,index);
+        }
+    }
+    /**
+     * select Left or Right union according to the situation of Top or Bottom
+     * +left; +right; +right+left
+     */
+    private void unionSelectionLR(int x, int y, int index){
+        if (y == 1) {
+            if (this.isOpen(x, y + 1)) {
+                site.union(xyTo1d(x, y + 1), index);
+                connectTop.union(xyTo1d(x, y + 1), index);
+            }
+        } else if (y == size) {
+            if (this.isOpen(x, y - 1)) {
+                site.union(xyTo1d(x, y - 1), index);
+                connectTop.union(xyTo1d(x, y - 1), index);
+            }
+        } else {
+            if (this.isOpen(x, y - 1)) {
+                site.union(xyTo1d(x, y - 1), index);
+                connectTop.union(xyTo1d(x, y - 1), index);
+            }
+            if (this.isOpen(x, y + 1)) {
+                site.union(xyTo1d(x, y + 1), index);
+                connectTop.union(xyTo1d(x, y + 1), index);
+            }
+        }
+    }
+    public boolean isOpen(int row, int col) {        // is site (row, col) open?
         validate(row, col);
         int index = xyTo1d(row, col);
-        return siteStatus[index];
+        return isOpenSite[index];
     }
-    public boolean isFull(int row, int col){        // is site (row, col) full?
+
+    public boolean isFull(int row, int col) {        // is site (row, col) full?
         validate(row, col);
         int index = xyTo1d(row, col);
-        if (site.connected(index, 0) || site.connected(index, site.count() - 1)) return true;
-        else return false;
+        return (isOpen(row, col) && connectTop.connected(index, 0));          //a backwash problem here
     }
-    public int numberOfOpenSites(){      // number of open sites
+    public int numberOfOpenSites() {      // number of open sites
         return nOS;
     }
-    public boolean percolates(){        // does the system percolate?
-        if (site.find(0) == site.find(site.count()-1)) return true;
-        else return false;
+    public boolean percolates() {        // does the system percolate?
+        return site.connected(0, size * size + 1);
     }
 
     /**
      * initialize the status array of concrete and virtual sites
      */
-    private boolean[] initSiteStatus(int n){
-        boolean[] a = new boolean[n];
-        a[0] = true;
-        a[a.length - 1] = true;
-        return a;
+    private void initSiteStatus(int n) {
+        this.isOpenSite = new boolean[n];
+        this.isOpenSite[0] = true;
+        this.isOpenSite[n-1] = true;
+        for (int i = 1; i <= size; i++) {
+            site.union(0, i);
+            connectTop.union(0, i);
+            site.union(size * size + 1, (size - 1) * size + i);
+        }
     }
 
     /**
      * convert 2-dimensional (row, col) to 1-dimensional index
+     *  1<=x,y<=size, 1<=return<=size^2
      */
-    private int xyTo1d(int x, int y){
+    private int xyTo1d(int x, int y) {
         return (x - 1) * size + y;
     }
 
     /**
      * validating the range of row & col
+     * 1<=x,y<=size
      */
-    private void validate(int x, int y){
+    private boolean validate(int x, int y){
         if (x < 1 || x > size) throw new IllegalArgumentException("row should be [1,n]");
         else if (y < 1 || y > size) throw new IllegalArgumentException("col should be [1,n]");
+        else return true;
     }
 
 }
--- Assignment/week1/TestPercolation.java
+++ Assignment/week1/TestPercolation.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.*;
 public class TestPercolation {
     @Test
     public void test1(){
+        Percolation percolation = new Percolation(5);
+
 
     }
 }

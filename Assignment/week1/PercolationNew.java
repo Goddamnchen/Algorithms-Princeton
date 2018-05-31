552fdd81560468e7042ca691b9f8372a92744d2a
Goddamnchen <goddamnchen@163.com>
2018-05-31 22:55:56 +0800

    PercolationNew(with no virtual site) version 2.1 update, but with unstatisfied memory and time usuage

--- Assignment/week1/PercolationNew.java
+++ Assignment/week1/PercolationNew.java
@@ -1,23 +1,30 @@
-/*****************************************************************
+/***********************************************************************
  *   Author: Guanting Chen
- *   Date: 05/29/2018
+ *   Date: 05/30/2018
  *   Union-Find Assignment
- *   Percolation Version 2.0 : Dealing backwater with status Array
- *   Note: this version do not use Top&Bottom Virtual Site
- *   API using by PercolationVisualizer
- *****************************************************************/
+ *   Percolation Version 2.1 :
+ *   1.fixed bug of n = 1 situation
+ *   2.refactoring percolates() and open() to not use virtual site
+ *   3.open() cost a little bit time more than Version2.0
+ *
+ *   Note: this version DO NOT use Top&Bottom Virtual Site
+ *   Worst percolates case runtime = ～ (0.59*20)nlogn + 6n (n=n*n grid)
+ *   API using by PercolationVisualizer and PercolationStats
+ ***********************************************************************/
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationNew {
     private WeightedQuickUnionUF site;
     private int size;
-    private int nOS;        //number of Open Sites
+    private int nOS;                        //number of Open Sites
     private boolean[] isOpenSite;
     private boolean[][] isConnectTB;        //通过union roots，记录每个union是否connect to top or bottom
-    public PercolationNew(int n) {      // create n-by-n grid, with all sites blocked
+    private boolean percolatedflag;         //using percolated to track status of Top and Bottom
+
+    public PercolationNew(int n) {          // create n-by-n grid, with all sites blocked
         if (n <= 0) throw new IllegalArgumentException("n can not < 0");
-        this.site = new WeightedQuickUnionUF(n * n + 1);       // implement index [0,n^2], with only Top Virtual site
+        this.site = new WeightedQuickUnionUF(n * n + 1);       // implement index [0,n^2], index 0 is not used
         this.size = n;
         this.initSiteStatus(n * n + 1);
     }
@@ -26,13 +33,14 @@ public class PercolationNew {
      * 2.label a open site (row, col) if it is not open already
      * 3.Get the existing component identifier status of 2 open union sets
      *   and then merge to a integra-l union and update the  status of this new union
+     * Takes 20logn time in worst case
      */
-    public void open(int row, int col){
+    public void open(int row, int col) {
         validate(row, col);
-        if (!isOpen(row, col)){
+        if (!isOpen(row, col)) {
             unionSelection(row, col);       //1<=index<=size*size 2;
-            isOpenSite[xyTo1d(row,col)] = true;
-            nOS ++;
+            isOpenSite[xyTo1d(row, col)] = true;
+            nOS++;
         }
     }
 
@@ -42,50 +50,56 @@ public class PercolationNew {
      * Dependency Principle: 每个union中的所有元素的Connect status 由该union 的ROOT STATUS代表
      */
     private void unionSelection(int x, int y) {
-        int index = xyTo1d(x,y);
-        if (x == 1) {                                   //on the top line
-            CheckandUnion(0, index);           /*top line element --initialize union--> top virtual element
-                                                        isConnectTB[top_line_index][0] --> isConnectTB[top_virtual_index][0] = true; */
+        int index = xyTo1d(x, y);
+        if (this.size == 1) {                       //special case, size = 1;
+            isConnectTB[index][0] = true;
+            isConnectTB[index][1] = true;
+            this.percolatedflag = true;
+            return;
+        }
+        if (x == 1) {
+            this.isConnectTB[index][0] = true;          /*initialize top line element to connect to top */
+
             if (this.isOpen(x + 1, y)) {            //+down
-                CheckandUnion(xyTo1d(x + 1,y), index);
+                checkUnion(xyTo1d(x + 1, y), index);
             }
-            unionSelectionLR(x,y,index);
-        } else if (x == size) {                         //on the bottom line
-            isConnectTB[index][1] = true;               //bottom line element --initialize status--> isConnectTB[bottom_line_index][1]
+            unionSelectionLR(x, y, index);
+        } else if (x == size) {
+            this.isConnectTB[index][1] = true;          /*initialize bottom line element to connect to bottom */
 
             if (this.isOpen(x - 1, y)) {           //+up
-                CheckandUnion(xyTo1d(x - 1,y), index);
+                checkUnion(xyTo1d(x - 1, y), index);
             }
-            unionSelectionLR(x,y,index);
+            unionSelectionLR(x, y, index);
         } else {                                       //+up+down
             if (this.isOpen(x - 1, y)) {
-                CheckandUnion(xyTo1d(x - 1,y), index);
+                checkUnion(xyTo1d(x - 1, y), index);
             }
             if (this.isOpen(x + 1, y)) {
-                CheckandUnion(xyTo1d(x + 1,y), index);
+                checkUnion(xyTo1d(x + 1, y), index);
             }
-            unionSelectionLR(x,y,index);
+            unionSelectionLR(x, y, index);
         }
     }
     /**
      * select Left or Right union according to the situation of Top or Bottom
      * +left; +right; +right+left
      */
-    private void unionSelectionLR(int x, int y, int cur_index){
-        if (y == 1) {           //+right
+    private void unionSelectionLR(int x, int y, int curIndex) {
+        if (y == 1) {                                  //+right
             if (this.isOpen(x, y + 1)) {
-                CheckandUnion(xyTo1d(x, y + 1), cur_index);
+                checkUnion(xyTo1d(x, y + 1), curIndex);
             }
-        } else if (y == size) {         //+left
+        } else if (y == size) {                        //+left
             if (this.isOpen(x, y - 1)) {
-                CheckandUnion(xyTo1d(x, y - 1), cur_index);
+                checkUnion(xyTo1d(x, y - 1), curIndex);
             }
-        } else {            //+left+right
+        } else {                                       //+left+right
             if (this.isOpen(x, y - 1)) {
-                CheckandUnion(xyTo1d(x, y - 1), cur_index);
+                checkUnion(xyTo1d(x, y - 1), curIndex);
             }
             if (this.isOpen(x, y + 1)) {
-                CheckandUnion(xyTo1d(x, y + 1), cur_index);
+                checkUnion(xyTo1d(x, y + 1), curIndex);
             }
         }
     }
@@ -95,20 +109,26 @@ public class PercolationNew {
      * @param neighbour representing the surround-site(Set) waited to be union
      * @param cur representing the center-site(Set) going to union other
      * !!The ROOT's CONNECT STATUS represent the same all status of a whole Set
+     * Takes 5logn time in worst case
      */
-    private void CheckandUnion(int neighbour, int cur){
+    private void checkUnion(int neighbour, int cur) {
         boolean top;
         boolean bottom;
-        if (isConnectTB[site.find(neighbour)][0] || isConnectTB[site.find(cur)][0]) top = true;
+        int neighbourRoot = site.find(neighbour);
+        int curRoot = site.find(cur);
+        if (isConnectTB[neighbourRoot][0] || isConnectTB[curRoot][0]) top = true;
         else top = false;
-        if ((isConnectTB[site.find(neighbour)][1] || isConnectTB[site.find(cur)][1])) bottom = true;
+        if ((isConnectTB[neighbourRoot][1] || isConnectTB[curRoot][1])) bottom = true;
         else bottom = false;
 
         site.union(neighbour, cur);         //union and then set the CONNECT STATUS of new set
         isConnectTB[site.find(cur)][0] = top;
         isConnectTB[site.find(cur)][1] = bottom;
+        if (top && bottom) {                // judging whether is percolated
+            this.percolatedflag = true;
+        }
     }
-    public boolean isOpen(int row, int col) {        // is site (row, col) open?
+    public boolean isOpen(int row, int col) {
         validate(row, col);
         int index = xyTo1d(row, col);
         return isOpenSite[index];
@@ -117,6 +137,7 @@ public class PercolationNew {
     /**
      * Judging whether a site is a Full open site
      * @return by checking the CONNECT STATUS of a whole set
+     * Takes logn time in worst case because of .find()
      */
     public boolean isFull(int row, int col) {
         validate(row, col);
@@ -129,11 +150,12 @@ public class PercolationNew {
 
     /**
      * Judging whether is percolates
-     * @return by checking the CONNECT STATUS of the root of a set containing Top Virtual Site;
-     * which represents a set must have bottom connected to the top
+     * Get rid of costly find() of top virtual site to check CONNECT STATUS
+     * @return percolatedflag which tracks CONNECT STATUS during Open()
+     * Takes constant time
      */
-    public boolean percolates() {        // does the system percolate?
-        return (isConnectTB[site.find(0)][0] && isConnectTB[site.find(0)][1]);
+    public boolean percolates() {
+        return this.percolatedflag;
     }
 
     /**
@@ -141,9 +163,7 @@ public class PercolationNew {
      */
     private void initSiteStatus(int n) {
         this.isOpenSite = new boolean[n];
-        this.isOpenSite[0] = true;
         this.isConnectTB = new boolean[n][2];
-        this.isConnectTB[0][0] = true;          //top virtual site is connected to top
     }
 
     /**
@@ -158,9 +178,9 @@ public class PercolationNew {
      * validating the range of row & col
      * 1<=x,y<=size
      */
-    private boolean validate(int x, int y){
-        if (x < 1 || x > size) throw new IllegalArgumentException("row should be [1,n]");
-        else if (y < 1 || y > size) throw new IllegalArgumentException("col should be [1,n]");
+    private boolean validate(int x, int y) {
+        if (x < 1 || x > this.size) throw new IllegalArgumentException("row should be [1,n]");
+        else if (y < 1 || y > this.size) throw new IllegalArgumentException("col should be [1,n]");
         else return true;
     }
 
